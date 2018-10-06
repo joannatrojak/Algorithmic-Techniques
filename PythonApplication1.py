7e9f8962710132db77f268b3925c26dd0b38fbce
Joanna Trojak <joasia405@gmail.com>
2018-10-06 14:41:55 +0200

    insertion sort

---
+++ PythonApplication1.py
@@ -0,0 +1,12 @@
+def insertionSort(array): 
+     for j in range(1,len(array)):
+         key = array[j]
+         i = j-1
+         while (i > -1) and key < array[i]:
+             array[i+1] = array[i]
+             i=i-1
+         array[i+1] = key
+     return array
+
+x = [5,2,4,6,1,3]
+print(insertionSort(x))
