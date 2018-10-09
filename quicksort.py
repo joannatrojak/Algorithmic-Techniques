8599900ea350832dc79c38bb5e6f46c41ae94bf2
joannatrojak <42413888+joannatrojak@users.noreply.github.com>
2018-10-09 08:02:04 +0200

    quick sort changed

--- quicksort.py
+++ quicksort.py
@@ -1,50 +1,45 @@
-import numpy as np
-from timeit import Timer
-import random as r
+def quick_sort(A):
+	quick_sort2(A, 0, len(A)-1)
+	
+def quick_sort2(A, low, hi):
+	if low < hi:
+		p = partition(A, low, hi)
+		quick_sort2(A, low, p - 1)
+		quick_sort2(A, p + 1, hi)
+	
+def get_pivot(A, low, hi):
+	mid = (hi + low) // 2
+	s = sorted([A[low], A[mid], A[hi]])
+	if s[1] == A[low]:
+		return low
+	elif s[1] == A[mid]:
+		return mid
+	return hi
+	
+def partition(A, low, hi):
+	pivotIndex = get_pivot(A, low, hi)
+	pivotValue = A[pivotIndex]
+	A[pivotIndex], A[low] = A[low], A[pivotIndex]
+	border = low
 
-def quickSort(array):
-    less = []
-    equal = []
-    greater = []
+	for i in range(low, hi+1):
+		if A[i] < pivotValue:
+			border += 1
+			A[i], A[border] = A[border], A[i]
+	A[low], A[border] = A[border], A[low]
 
-    if len(array) > 1:
-        pivot = array[0]
-        for x in array:
-            if x < pivot:
-                less.append(x)
-            if x == pivot:
-                equal.append(x)
-            if x > pivot:
-                greater.append(x)
-        return quickSort(less) + equal + quickSort(greater)
-    else:
-        return array
-
-def quickSort2(array):
-    less = []
-    equal = []
-    greater = []
-
-    if len(array) > 1:
-        pivot = array[r.randint(1, 100)]
-        print(pivot)
-        for x in array:
-            if x < pivot:
-                less.append(x)
-            if x == pivot:
-                equal.append(x)
-            if x > pivot:
-                greater.append(x)
-        return quickSort(less) + equal + quickSort(greater)
-    else:
-        return array
-
-array = np.random.randint(1, 1000, 10)
-print(quickSort(array))
-print(quickSort2(array))
-t = Timer(lambda: quickSort(array))
-s = Timer(lambda: quickSort2(array))
-print(t.timeit(number=1))
-print(s.timeit(number=1))
-
-
+	return (border)
+	
+def quick_selection(x, first, last):
+	for i in range (first, last):
+		minIndex = i
+		for j in range (i+1, last+1):
+			if x[j] < x[minIndex]:
+				minIndex = j
+		if minIndex != i:
+			x[i], x[minIndex] = x[minIndex], x[i]
+			
+A = [5,9,1,2,4,8,6,3,7]
+print(A)
+quick_sort(A)
+print(A)
